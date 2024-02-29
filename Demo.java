5eaa6065a2b6eb7426bd72f71f0fe6f16195b7f7
akash <akash300383@gmail.com>
2024-02-29 08:43:10 +0530

    done

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Welcome to Simple Jenkin Job");
+        System.out.println("Welcome to Simple Jenkin Job")
     }
 }
