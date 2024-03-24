19a6baa753f75543d9561fecf61c20f5074ca73c
akash <akash300383@gmail.com>
2024-03-24 21:25:16 +0530

    done

--- Demo.java
+++ Demo.java
@@ -2,5 +2,5 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Welcome to Simple Jenkin Job");
         System.out.println("Jenkin pipeline job with post build action");
-    
+    }
 }
